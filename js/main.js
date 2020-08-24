ad4176d710617edb5af1fe560f4680fbaac68510
LSC <lelandscrawford@gmail.com>
2020-08-24 11:18:33 -0400

    added catch

--- js/main.js
+++ js/main.js
@@ -80,83 +80,92 @@ function getWeather(unit){
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?${locData}${units}&appid=${key}`)
     .then(response => response.json())
+    .catch(error => {
+      console.log('error at the beginning');
+    })
     .then(rawData =>{
 
-        // Local
-        let locCity = document.querySelector('#locCity');
-        let webcity = rawData.name;
-        locCity.innerHTML = webcity;
+      // Local
+      let locCity = document.querySelector('#locCity');
+      let webcity = rawData.name;
+      locCity.innerHTML = webcity;
 
 
-        let locState = document.querySelector('#locState');
-        locState.innerHTML = state;
+      let locState = document.querySelector('#locState');
+      locState.innerHTML = state;
 
-        let locZip = document.querySelector('#locZip');
-        locZip.innerHTML = zip;
+      let locZip = document.querySelector('#locZip');
+      locZip.innerHTML = zip;
 
-        let locCountry = document.querySelector('#locCountry');
-        let webcountry = rawData.sys.country;
-        locCountry.innerHTML = webcountry;
+      let locCountry = document.querySelector('#locCountry');
+      let webcountry = rawData.sys.country;
+      locCountry.innerHTML = webcountry;
 
-        // Temp
-        let currentTemp = document.querySelector('#currentTemp')
-        let web_temp = rawData.main.temp;
-        currentTemp.innerHTML = web_temp + " " +unit;
+      // Temp
+      let currentTemp = document.querySelector('#currentTemp')
+      let web_temp = rawData.main.temp;
+      currentTemp.innerHTML = web_temp + " " +unit;
 
-        // feelsLike Temp
-        let feelsTemp = document.querySelector('#feelsTemp');
-        let web_feels = rawData.main.feels_like;
-        feelsTemp.innerHTML = web_feels + " " +unit;
+      // feelsLike Temp
+      let feelsTemp = document.querySelector('#feelsTemp');
+      let web_feels = rawData.main.feels_like;
+      feelsTemp.innerHTML = web_feels + " " +unit;
 
-        // High Temp
-        let high = document.querySelector('#high');
-        let web_high = rawData.main.temp_max;
-        high.innerHTML = web_high + " " +unit;
+      // High Temp
+      let high = document.querySelector('#high');
+      let web_high = rawData.main.temp_max;
+      high.innerHTML = web_high + " " +unit;
 
-        // Low Temp
-        let low = document.querySelector('#low');
-        let web_low = rawData.main.temp_min;
-        low.innerHTML = web_low + " " +unit;
+      // Low Temp
+      let low = document.querySelector('#low');
+      let web_low = rawData.main.temp_min;
+      low.innerHTML = web_low + " " +unit;
 
-        // Amtmoshpecial Pressure
-        let ATM = document.querySelector('#ATM');
-        let web_ATM = rawData.main.pressure;
-        ATM.innerHTML = web_ATM + " hPa";
+      // Amtmoshpecial Pressure
+      let ATM = document.querySelector('#ATM');
+      let web_ATM = rawData.main.pressure;
+      ATM.innerHTML = web_ATM + " hPa";
 
-        // forecast
-        let forecast = document.querySelector('#forecast');
-        let webforecast_Main = rawData.weather[0].main;
-        let webforecast_Description = rawData.weather[0].description;
-        forecast.innerHTML = webforecast_Main  + ", \n" + webforecast_Description;
+      // forecast
+      let forecast = document.querySelector('#forecast');
+      let webforecast_Main = rawData.weather[0].main;
+      let webforecast_Description = rawData.weather[0].description;
+      forecast.innerHTML = webforecast_Main  + ", \n" + webforecast_Description;
 
-        // Humidity
-        let humidity = document.querySelector('#humid');
-        let web_humidity = rawData.main.humidity;
-        humidity.innerHTML = web_humidity  + "%" 
-        console.log(webID)
-        
-        // Windspeed
-        let wind = document.querySelector('#wind');
-        let web_wind_speed = rawData.wind.speed;
-        let web_wind_direction = rawData.wind.deg;
-        let windy = web_wind_speed + " " + speed + ", in a direction of " + web_wind_direction;
-        console.log(windy);
-        wind.innerHTML = windy;
+      // Humidity
+      let humidity = document.querySelector('#humid');
+      let web_humidity = rawData.main.humidity;
+      humidity.innerHTML = web_humidity  + "%" 
+      console.log(webID)
+      
+      // Windspeed
+      let wind = document.querySelector('#wind');
+      let web_wind_speed = rawData.wind.speed;
+      let web_wind_direction = rawData.wind.deg;
+      let windy = web_wind_speed + " " + speed + ", in a direction of " + web_wind_direction;
+      console.log(windy);
+      wind.innerHTML = windy;
 
 
-        // Sunrise and Sunset
-        let sunrise = document.querySelector('#sunrise');
-        let web_rise = rawData.sys.sunrise;
-        let web_sunrise =  new Date(web_rise *1000);
-        console.log(sunrise);
-        let sunset = document.querySelector('#sunset');
-        let web_set = rawData.sys.sunset;
-        let web_sunset = new Date(web_set *1000);
-        console.log(sunset);
-        // console.log(sunhours);
-        sunrise.innerHTML = "Sunrise: " + web_sunrise;
-        sunset.innerHTML = "Sunset: " + web_sunset;
-    });
+      // Sunrise and Sunset
+      let sunrise = document.querySelector('#sunrise');
+      let web_rise = rawData.sys.sunrise;
+      let web_sunrise =  new Date(web_rise *1000);
+      console.log(sunrise);
+      let sunset = document.querySelector('#sunset');
+      let web_set = rawData.sys.sunset;
+      let web_sunset = new Date(web_set *1000);
+      console.log(sunset);
+      // console.log(sunhours);
+      sunrise.innerHTML = "Sunrise: " + web_sunrise;
+      sunset.innerHTML = "Sunset: " + web_sunset;
+  })
+  .catch(error => {
+    console.log('error at the end')
+    let err = document.querySelector('#error');
+    let errMessage = "Location not recognized. \nChecking the infomation \nor adding more.";
+    err.innerHTML = errMessage;
+  })
 };
 
 
